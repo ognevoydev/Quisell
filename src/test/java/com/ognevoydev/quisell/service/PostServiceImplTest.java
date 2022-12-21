1e2c492513a66e2adc02e7a9af8b14dcfa3d93df
ognevoydev <ognevoy.devmail@gmail.com>
2022-12-21 19:56:53 +0300

    h2 docker

--- src/test/java/com/ognevoydev/quisell/service/PostServiceImplTest.java
+++ src/test/java/com/ognevoydev/quisell/service/PostServiceImplTest.java
@@ -6,11 +6,13 @@ import com.ognevoydev.quisell.repository.PostRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
+@ActiveProfiles("test")
 class PostServiceImplTest {
 
     @Autowired
